abeba527b57088f60fbef15af473cfd85203a902
RobsonPereira13 <robsonpereirape@hotmail.com>
2019-09-16 15:21:52 -0300

    primeiro commit

---
+++ target/generated-sources/annotations/com/projeto/dashfi_t/Card_.java
@@ -0,0 +1,16 @@
+package com.projeto.dashfi_t;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-16T15:15:50")
+@StaticMetamodel(Card.class)
+public class Card_ { 
+
+    public static volatile SingularAttribute<Card, String> etiqueta;
+    public static volatile SingularAttribute<Card, Integer> idCard;
+    public static volatile SingularAttribute<Card, String> titulo;
+    public static volatile SingularAttribute<Card, Integer> modelo;
+
+}
---
+++ target/generated-sources/annotations/com/projeto/dashfi_t/Valor_.java
@@ -0,0 +1,18 @@
+package com.projeto.dashfi_t;
+
+import java.math.BigInteger;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-16T15:15:50")
+@StaticMetamodel(Valor.class)
+public class Valor_ { 
+
+    public static volatile SingularAttribute<Valor, String> etiqueta;
+    public static volatile SingularAttribute<Valor, String> observacao;
+    public static volatile SingularAttribute<Valor, Integer> idValor;
+    public static volatile SingularAttribute<Valor, BigInteger> valor;
+    public static volatile SingularAttribute<Valor, String> titulo;
+
+}
